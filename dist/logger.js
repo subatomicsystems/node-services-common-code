6c21cedfd8afceeb5fd945b8ebef9374be014746
Bert Verhelst <verhelstbert@gmail.com>
2018-03-30 17:19:43 +0200

    Avoid throwing errors in bunyan can't write log files

--- dist/logger.js
+++ dist/logger.js
@@ -4,52 +4,58 @@ const bunyan = require("bunyan");
 const path = require("path");
 const fs = require("fs");
 const env_config_1 = require("./env-config");
-const createCWStream = require('bunyan-cloudwatch');
-const parent = require('parent-package-json');
-let pkg = null;
-let localPackagePath = path.join(process.cwd(), 'package.json');
-if (fs.existsSync(localPackagePath)) {
-    pkg = require(localPackagePath);
-}
-else {
-    let parentPackageJsonPath = parent(process.cwd());
-    if (parentPackageJsonPath) {
-        pkg = parent().parse();
-    }
-}
-if (!pkg || pkg.name === 'node-services-common-code') {
-    pkg = { name: 'could-not-find-package-json' };
-}
-let streams = [];
-if (process.env['LOG'] !== 'false') {
-    streams = [{
-            stream: process.stdout
-        }];
-}
-if (env_config_1.default.env !== 'development') {
-    const cloudWatchStream = createCWStream({
-        logGroupName: `/node-apis/${env_config_1.default.env}`,
-        logStreamName: pkg.name,
-        cloudWatchLogsOptions: {
-            region: 'us-west-1'
-        }
-    });
-    streams.push({
-        stream: cloudWatchStream,
-        type: 'raw',
-        level: 'info'
-    });
-}
-const logger = bunyan.createLogger({
-    name: pkg.name,
-    streams
-});
+let logger;
 exports.logger = logger;
-process.on('uncaughtException', (err) => {
-    logger.fatal('uncaught exception', err);
-});
-process.on('SIGTERM', () => {
-    logger.fatal('SIGTERM');
-});
+try {
+    const createCWStream = require('bunyan-cloudwatch');
+    const parent = require('parent-package-json');
+    let pkg = null;
+    let localPackagePath = path.join(process.cwd(), 'package.json');
+    if (fs.existsSync(localPackagePath)) {
+        pkg = require(localPackagePath);
+    }
+    else {
+        let parentPackageJsonPath = parent(process.cwd());
+        if (parentPackageJsonPath) {
+            pkg = parent().parse();
+        }
+    }
+    if (!pkg || pkg.name === 'node-services-common-code') {
+        pkg = { name: 'could-not-find-package-json' };
+    }
+    let streams = [];
+    if (process.env['LOG'] !== 'false') {
+        streams = [{
+                stream: process.stdout
+            }];
+    }
+    if (env_config_1.default.env !== 'development') {
+        const cloudWatchStream = createCWStream({
+            logGroupName: `/node-apis/${env_config_1.default.env}`,
+            logStreamName: pkg.name,
+            cloudWatchLogsOptions: {
+                region: 'us-west-1'
+            }
+        });
+        streams.push({
+            stream: cloudWatchStream,
+            type: 'raw',
+            level: 'info'
+        });
+    }
+    exports.logger = logger = bunyan.createLogger({
+        name: pkg.name,
+        streams
+    });
+    process.on('uncaughtException', (err) => {
+        logger.fatal('uncaught exception', err);
+    });
+    process.on('SIGTERM', () => {
+        logger.fatal('SIGTERM');
+    });
+}
+catch (err) {
+    console.log('[node-services-common-code] Failed to initialize logger', err);
+}
 exports.default = logger;
 //# sourceMappingURL=logger.js.map
--- logger.ts
+++ logger.ts
@@ -4,63 +4,71 @@ import * as bunyan from 'bunyan';
 import * as path from 'path';
 import * as fs from 'fs';
 import config from './env-config';
-const createCWStream = require('bunyan-cloudwatch');
 
-const parent = require('parent-package-json');
+let  logger: any;
 
-let pkg: {name: string} | null = null;
-let localPackagePath = path.join(process.cwd(), 'package.json');
-if (fs.existsSync(localPackagePath)) {
-  pkg = require(localPackagePath);
-} else {
-  let parentPackageJsonPath: any = parent(process.cwd());
-  if (parentPackageJsonPath) {
-    pkg = parent().parse();
+try {
+  const createCWStream = require('bunyan-cloudwatch');
+
+  const parent = require('parent-package-json');
+
+  let pkg: { name: string } | null = null;
+  let localPackagePath = path.join(process.cwd(), 'package.json');
+  if (fs.existsSync(localPackagePath)) {
+    pkg = require(localPackagePath);
+  } else {
+    let parentPackageJsonPath: any = parent(process.cwd());
+    if (parentPackageJsonPath) {
+      pkg = parent().parse();
+    }
   }
-}
-if (!pkg || pkg.name === 'node-services-common-code') {
-  pkg = {name: 'could-not-find-package-json'};
-}
+  if (!pkg || pkg.name === 'node-services-common-code') {
+    pkg = {name: 'could-not-find-package-json'};
+  }
 
 // array of streams
-let streams: any[] = [];
+  let streams: any[] = [];
 
-if (process.env['LOG'] !== 'false') {
-  streams = [{
-    stream: process.stdout
-  }];
-}
+  if (process.env['LOG'] !== 'false') {
+    streams = [{
+      stream: process.stdout
+    }];
+  }
 
 // we don't want to log to CloudWatch for local development
-if (config.env !== 'development') {
-  // create a stream that pipes it into cloudwatch
-  const cloudWatchStream = createCWStream({
-    logGroupName: `/node-apis/${config.env}`,
-    logStreamName: pkg.name,
-    cloudWatchLogsOptions: {
-      region: 'us-west-1'
-    }
-  });
-  streams.push({
-    stream: cloudWatchStream,
-    type: 'raw',
-    level: 'info'
-  });
-}
+  if (config.env !== 'development') {
+    // create a stream that pipes it into cloudwatch
+    const cloudWatchStream = createCWStream({
+      logGroupName: `/node-apis/${config.env}`,
+      logStreamName: pkg.name,
+      cloudWatchLogsOptions: {
+        region: 'us-west-1'
+      }
+    });
+    streams.push({
+      stream: cloudWatchStream,
+      type: 'raw',
+      level: 'info'
+    });
+  }
 
-const logger = bunyan.createLogger({
-  name: pkg.name,
-  streams
-});
+  logger = bunyan.createLogger({
+    name: pkg.name,
+    streams
+  });
 
 // Gracefully handle the server being killed by external processes like NodeMon so that the port is also closed.
-process.on('uncaughtException', (err: any) => {
-  logger.fatal('uncaught exception', err);
-});
+  process.on('uncaughtException', (err: any) => {
+    logger.fatal('uncaught exception', err);
+  });
 
-process.on('SIGTERM', () => {
-  logger.fatal('SIGTERM');
-});
+  process.on('SIGTERM', () => {
+    logger.fatal('SIGTERM');
+  });
+
+} catch (err) {
+  console.log('[node-services-common-code] Failed to initialize logger', err);
+}
 
 export { logger };
 export default logger;
