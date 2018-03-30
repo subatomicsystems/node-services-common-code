915ec64797ab47fe24e08832e8f923e68712d7c1
Bert Verhelst <verhelstbert@gmail.com>
2018-03-30 17:09:57 +0200

    Remove logger from environment config
    otherwise we get: read-only file system, open 'info.log' on lambda

--- dist/env-config.js
+++ dist/env-config.js
@@ -1,7 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const _ = require("lodash");
-const logger_1 = require("./logger");
 let env = process.argv.indexOf('-env') === -1 ? (process.env['ENV'] || 'development') : process.argv[process.argv.indexOf('-env') + 1];
 let configJson = {};
 try {
@@ -11,19 +10,11 @@ catch (err) {
     console.warn('No config.json file was found for this repository');
 }
 let env_Config = configJson[env] || {};
-let hasInstanceNumber = process.argv.indexOf('-instance') !== -1;
 let envConfig = _.merge(configJson, env_Config);
 exports.envConfig = envConfig;
 if (!envConfig.base) {
     envConfig.base = '';
 }
 envConfig.env = env;
-if (hasInstanceNumber) {
-    let instanceNumber = parseInt(process.argv[process.argv.indexOf('-instance') + 1]);
-    envConfig.port += instanceNumber;
-    if (instanceNumber > 9) {
-        logger_1.logger.warn('Number of instances is expected not to exceed 9 because of possible port conflicts with other services.');
-    }
-}
 exports.default = envConfig;
 //# sourceMappingURL=env-config.js.map
